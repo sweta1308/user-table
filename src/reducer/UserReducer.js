0d143db4ddfc6abf77e441a680bf285dee008133
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-03 19:32:19 +0530

    feat: add user reducer

---
+++ src/reducer/UserReducer.js
@@ -0,0 +1,60 @@
+export const userReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "LOADING":
+      return { ...state, isLoading: payload };
+    case "SET_USERS":
+      return { ...state, users: payload };
+    case "UPDATE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === payload.id ? payload : user
+        ),
+      };
+    case "SET_EDIT_ID":
+      return { ...state, editId: payload };
+    case "DELETE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== payload),
+        filters: {
+          ...state.filters,
+          checkedItems: state.filters.checkedItems.filter(
+            (item) => item !== payload
+          ),
+        },
+      };
+    case "SET_CURRENT_PAGE":
+      return { ...state, currentPage: payload };
+    case "SEARCH_USERS":
+      return { ...state, filters: { ...state.filters, searchTerm: payload } };
+    case "CHECKED_ITEMS":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          checkedItems: state.filters.checkedItems.includes(payload)
+            ? state.filters.checkedItems.filter((item) => item !== payload)
+            : [...state.filters.checkedItems, payload],
+        },
+      };
+    case "SELECT_ALL":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          checkedItems: payload,
+        },
+      };
+    case "DELETE_ALL":
+      return {
+        ...state,
+        users: state.users.filter(
+          ({ id }) => !state.filters.checkedItems.includes(id)
+        ),
+        filters: { ...state.filters, checkedItems: [] },
+      };
+    default:
+      return state;
+  }
+};
